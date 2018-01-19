afa07669b675d6e3bde42e70d38867738401c568
Tijana Vislavski <tijana.vislavski@dmi.uns.ac.rs>
2018-01-19 12:08:42 +0100

    added Primer.java

---
+++ TreciKolokvijumB/src/Primer.java
@@ -0,0 +1,25 @@
+
+
+public class Primer {
+	/**
+	 * Count odd or postive elements
+	 *
+	 * @param x
+	 *            array to search
+	 * @return count of odd/positive values in x
+	 * @throws NullPointerException
+	 *             if x is null
+	 */
+	public int oddOrPos(int[] x) {
+		int count = 0;
+		for (int i = 0; i < x.length - 1; i++) {
+			if (x[i] % 2 == 1 && x[i] > 0) {
+				count++;
+			}
+		}
+		return count;
+	}
+	// test: x = [-3, -2, 0, 1, 4]; Expected = 3
+
+}
+
