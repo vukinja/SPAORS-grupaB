1c2506fd2d3eee8a26775d86ebfd039aaa9f736e
vukinja <vukasinsmiovic@gmail.com>
2020-05-29 13:55:34 +0200

    Popravljen oddOrPos - opseg vrednosti u for petlji

--- TreciKolokvijumB/src/Primer.java
+++ TreciKolokvijumB/src/Primer.java
@@ -12,7 +12,7 @@ public class Primer {
 	 */
 	public int oddOrPos(int[] x) {
 		int count = 0;
-		for (int i = 0; i < x.length - 1; i++) {
+		for (int i = 0; i > x.length - 1; i++) {
 			if (x[i] % 2 == 1 && x[i] > 0) {
 				count++;
 			}
